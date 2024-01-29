a7cb0e5a9f7189cb5e240a69289a7328ec6559da
willwooten <wwootencfc@gmail.com>
2024-01-29 16:39:24 -0600

    edit directions

--- src/99_tests.py
+++ src/99_tests.py
@@ -9,7 +9,22 @@ sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 with open("test/haiko.json", 'r') as file:
     haiko = json.load(file)
 
+
+#####################################
+##### HAIKO TEST ####################
+#####################################
+    
 with open("test/test_haiko.json") as file:
     test_json_haiko = json.load(file)
 
 assert(haiko == test_json_haiko)
+
+#####################################
+##### ADD EKUBO TEST HERE ###########
+#####################################
+
+with open("test/test_ekubo.json") as file:
+    test_json_haiko = json.load(file)
+
+assert(haiko == test_json_haiko)
+
